bee80231874c9221ffb746ea2cf1b0685538c581
Ethan Tran <ethantran@ethans-mbp-2.lan>
2025-07-10 22:50:35 -0600

    Modified app.jsx file and made footer and header file.

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1>J n Buh Barber Website</h1>
+            <nav>
+                <ul>
+                    <li><a href = "#">Home</a></li>
+                    <li><a href = "#">About</a></li>
+                    <li><a href = "#">Appointment Booking</a></li>
+                    <li><a href = "#">Haircuts</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+
+    );
+
+}
+
+export default Header
